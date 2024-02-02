e3c11e287af98a04eb361f564cf663e29956a078
Yevhenii Korzun <yevhenii.korzun.job@gmail.com>
2024-02-02 17:14:52 +0200

    fetch data with weather module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WeatherService } from './weather/weather.service';
+import { WeatherModule } from './weather/weather.module';
+import {HttpModule} from "@nestjs/axios";
 
 @Module({
-  imports: [],
+  imports: [WeatherModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WeatherService],
 })
 export class AppModule {}
